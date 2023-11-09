6eb441823a5f27f63e813b87b3f577d281cde1aa
Ansuman Singh <ansumansingh@Ansumans-MacBook-Pro.local>
2023-11-09 11:45:11 -0600

    visits: docker-compose

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const redis = require('redis');
+
 
 const app = express();
+const client = redis.createClient({
+	host:'my-redis',
+	port: 6379
+})
+client.set('visits', 0)
 
 app.get('/', (req,res) => 
 { 
-	res.send("<h1>Hello World!</h1>");
-})
+	client.get('visits', (err, visits)=>{
+		res.send("Number of visits" + visits)
+		client.set('visits', parseInt(visits)+1)
+	});
+});
 
 
 app.listen(8080, ()=> {
